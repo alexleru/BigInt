2099a5107b881706b475d6b0b077566034b4b866
alexleru <alexleru@yandex.ru>
2019-02-08 21:33:39 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BigInteger bigInteger = new BigInteger("1045646044789465465464111111111111111111111111590");
+
+        System.out.println(bigInteger.divide(new BigInteger("2")));
+    }
+}
