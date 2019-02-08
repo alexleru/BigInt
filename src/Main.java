f895bd0522fead447d7446cb7bd993b5154e1cfc
alexleru <alexleru@yandex.ru>
2019-02-08 23:34:12 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        BigInteger bigInteger = new BigInteger("1045646044789465465464111111111111111111111111590");
-
-        System.out.println(bigInteger.divide(new BigInteger("2")));
+        BigInteger bigInteger = new BigInteger("3");
+        ArrayList<BigInteger> bigIntegers = new ArrayList<>();
+        bigIntegers.add(bigInteger);
+        PrimeNumbers primeNumbers = new PrimeNumbers(bigIntegers, new BigInteger("5"));
+        primeNumbers.loop();
     }
 }
